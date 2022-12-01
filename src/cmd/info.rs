f57b58de0c26cd7bc1f8b6469edfccf9fb25b97a
Matthias Stangl <matze749@gmail.com>
2022-12-01 23:36:19 +0100

    Move reading of information to data module
    Move the read information implementation to the data module.
    This was previously part of the info command module.

--- src/cmd/info.rs
+++ src/cmd/info.rs
@@ -1,13 +1,8 @@
-use std::{fs, path::PathBuf};
+use std::path::PathBuf;
 
 use clap::Args;
-use object::{FileFlags, Object, ObjectSection, ObjectSymbol, SectionFlags};
-use uuid::Uuid;
 
-use crate::{
-    data::info::{CoffFileFlags, CoffSectionFlags, Information, PdbInfo, Section, Symbol},
-    output::PrintOutput,
-};
+use crate::{data::info::Information, output::PrintOutput};
 
 // Command line arguments for the info module.
 #[derive(Args)]
@@ -17,75 +12,5 @@ pub struct Arguments {
 }
 
 pub fn run(arguments: &Arguments, output: &dyn PrintOutput) {
-    let binary_data = fs::read(&arguments.file).unwrap();
-    let object_file = object::File::parse(&*binary_data).unwrap();
-
-    // File flags for PE/COFF
-    let flags = if let FileFlags::Coff { characteristics: c } = object_file.flags() {
-        CoffFileFlags::from_bits(c)
-    } else {
-        None
-    };
-
-    // PDB infos
-    let pdb = if let Ok(Some(pdb_info)) = object_file.pdb_info() {
-        Some(PdbInfo {
-            age: pdb_info.age(),
-            guid: Uuid::from_bytes(pdb_info.guid()).hyphenated().to_string(),
-            path: String::from_utf8_lossy(pdb_info.path()).into(),
-        })
-    } else {
-        None
-    };
-
-    // Sections
-    let mut sections = Vec::<Section>::new();
-    for section in object_file.sections() {
-        let flags = if let SectionFlags::Coff { characteristics: c } = section.flags() {
-            CoffSectionFlags::from_bits(c)
-        } else {
-            None
-        };
-
-        let segment_name = if let Ok(Some(name)) = section.segment_name() {
-            Some(name.to_string())
-        } else {
-            None
-        };
-
-        sections.push(Section {
-            address: section.address(),
-            name: section.name().unwrap_or_default().to_string(),
-            kind: section.kind(),
-            size: section.size(),
-            segment_name,
-            coff_section_flags: flags,
-        });
-    }
-
-    // Symbols
-    let mut symbols = Vec::<Symbol>::new();
-    for symbol in object_file.symbols() {
-        symbols.push(Symbol {
-            address: symbol.address(),
-            kind: symbol.kind(),
-            name: symbol.name().unwrap_or_default().to_owned(),
-            scope: symbol.scope(),
-            section: symbol.section(),
-            size: symbol.size(),
-        });
-    }
-
-    output.print_information(&Information {
-        architecture: object_file.architecture(),
-        endianess: object_file.endianness(),
-        is_64: object_file.is_64(),
-        kind: object_file.kind(),
-        has_debug_symbols: object_file.has_debug_symbols(),
-        entry_address: object_file.entry(),
-        coff_file_flags: flags,
-        pdb_info: pdb,
-        sections,
-        symbols,
-    });
+    output.print_information(&Information::read(&arguments.file));
 }
